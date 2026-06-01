3e3ba8eb8fff36f5b8e1f282c40dfad2a806043c
Nirbhai singh <120583283+singhnirbhai@users.noreply.github.com>
2026-06-02 00:07:58 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+/*const API = axios.create({ baseURL: 'http://localhost:5000/api' });*/
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || '/api'
+});
 
 API.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem('user'));
